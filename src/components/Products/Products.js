cc0c34238d3ae786cf9e586269e3eb792caf0e32
Maruf Islam <maruf15-1591@diu.edu.bd>
2021-09-26 04:14:36 +0600

    add home page spinner

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -1,19 +1,34 @@
 import React, { useEffect, useState } from "react";
+import { Spinner } from "react-bootstrap";
 import "./Products.css";
 import ProductsContent from "./ProductsContent";
 
 const Products = () => {
   const [addedProducts, setAddedProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     fetch("https://trend-hub-server.herokuapp.com/newProducts")
       .then((res) => res.json())
-      .then((data) => setAddedProducts(data));
+      .then((data) => {
+        setAddedProducts(data);
+        setLoading(false);
+      });
   }, []);
   return (
-    <div className="product-container">
-      {addedProducts.map((pd) => (
-        <ProductsContent key={pd._id} productDetails={pd}></ProductsContent>
-      ))}
+    <div>
+      {loading && (
+        <div className="spinner-container">
+          <Spinner animation="border" variant="danger" />
+          <Spinner animation="border" variant="danger" />
+          <Spinner animation="border" variant="danger" />
+          <Spinner animation="border" variant="danger" />
+        </div>
+      )}
+      <div className="product-container">
+        {addedProducts.map((pd) => (
+          <ProductsContent key={pd._id} productDetails={pd}></ProductsContent>
+        ))}
+      </div>
     </div>
   );
 };
